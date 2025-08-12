6c58d33cc1e8292f0b42324f01908aea7ff988b4
pratishtharaj07 <pratishtharaj07@gmail.com>
2025-08-13 01:58:03 +0530

    PROGRAM 2

---
+++ assignment 2.py
@@ -0,0 +1,15 @@
+#assignment number 2
+#to check the temperature category
+
+
+temp=float(input("enter the temperature in celsius:  "))
+
+if temp<15:
+    print ("cold")
+elif 15<= temp <=25:
+    print ("warm")
+else:
+    print("hot")
+    
+
+    
