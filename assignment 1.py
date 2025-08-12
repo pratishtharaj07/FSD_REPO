10d4fb209e67210fa092af5b7bfa9a6cb4fcfdc4
pratishtharaj07 <pratishtharaj07@gmail.com>
2025-08-13 01:33:13 +0530

    PROGRAM1

---
+++ assignment 1.py
@@ -0,0 +1,10 @@
+#program 1
+#conditional statement - even odd
+
+
+value= int(input("enter a number:  "))
+if value%2==0:
+    print(f"{value}is an even number")
+else:
+    print(f"{value}is an odd number")
+                    
