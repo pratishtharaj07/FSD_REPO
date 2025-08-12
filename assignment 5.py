ed66520cf75f0e82c9a488111fda21205e6704de
pratishtharaj07 <pratishtharaj07@gmail.com>
2025-08-13 02:56:42 +0530

    PROGRAM 5

---
+++ assignment 5.py
@@ -0,0 +1,8 @@
+#function to calculate the square of a no.
+
+#assignment 5
+
+def calc_sq(number):
+    return number**2
+num=float(input("enter a number to calculate it's square: "))
+print(f" the square of {num}is {calc_sq(num)}")
